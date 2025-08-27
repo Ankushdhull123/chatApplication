cb92db76add983960f4f66e691c5960fea4eec1e
Ankushdhull123 <dhullstudent@gmail.com>
2025-08-27 21:42:57 +0530

    Update RoomPage.jsx

--- client/src/Pages/RoomPage.jsx
+++ client/src/Pages/RoomPage.jsx
@@ -56,7 +56,7 @@ function RoomPage() {
 
     async function addtogroup(name) {
   try {
-    const res = await fetch("http://localhost:5000/addmembertogroup", {
+    const res = await fetch("https://chatapplication-jxtt.onrender.com/addmembertogroup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
